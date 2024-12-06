ce450838fe62872768cd7dc6b11e6640e7a3df66
Herinirina Livasoa Razafiandriatsima <hrazafia@c1r3s5.42antananarivo.mg>
2024-12-06 15:01:53 +0300

    fix dist

--- RayCaster/src/main.c
+++ RayCaster/src/main.c
@@ -82,6 +82,15 @@ float distance(float x, float y)
 	return (sqrt(x * x + y * y));
 }
 
+float fixed_dist(float x1, float y1, float x2, float y2, t_game *game)
+{
+	float delta_x = x2 - x1;
+	float delta_y = y2 - y1;
+	float angle = atan2(delta_y, delta_x) - game->player.angle;
+	float fix_dist = distance(delta_x, delta_y) * cos(angle);
+	return (fix_dist);
+}
+
 void	draw_line(t_player *player, t_game *game, float start_x, int i)
 {
 	float cos_angle = cos(start_x);
@@ -91,11 +100,11 @@ void	draw_line(t_player *player, t_game *game, float start_x, int i)
 	
 	while (!touch(ray_x, ray_y, game))
 	{
-		put_pixel(ray_x, ray_y, 0xFF0000, game);
+		//put_pixel(ray_x, ray_y, 0xFF0000, game);
 		ray_x += cos_angle;
 		ray_y += sin_angle;
 	}
-	float dist = distance(ray_x - player->x, ray_y - player->y);
+	float dist = fixed_dist(player->x, player->y, ray_x, ray_y, game);
 	float height = (BLOCK / dist) * (WIDTH / 2);
 	int start_y = (HEIGHT - height) / 2;
 	int end = start_y + height;
@@ -111,8 +120,8 @@ int	draw_loop(t_game *game)
 	t_player *player = &game->player;
 	move_player(player);
 	clear_image(game);
-        draw_square(player->x, player->y, 10, 0x00FF00, game);
-        draw_map(game);
+        //draw_square(player->x, player->y, 10, 0x00FF00, game);
+        //draw_map(game);
 
 	float fraction = PI / 3 / WIDTH;
 	float start_x = player->angle - PI / 6;
